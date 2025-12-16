d7625cf8610140de14940010da78689be9882488
Bishal705 <bishalstha5777@gmail.com>
2025-12-16 02:30:17 +0000

    Add files via upload

---
+++ App.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Book_Form from "./components/AddBook";
+import Book_UpdateForm from "./components/BookUpdate";
+import FncDisplayBooks from "./components/DsplyBk_fncCompt";
+import DeleteBook from "./components/Delete_Book";
+
+function App() {
+  return (
+    <Router>
+      <div className="container">
+        <center>
+          <h2>On-Line Book Library using React</h2>
+        </center>
+        <br />
+        <nav className="navbar navbar-expand-lg navbar-light bg-success">
+          <Link to="/" className="navbar-brand">
+            <h4>Add a Book</h4>
+          </Link>
+          <Link to="/DisplayBooksF1" className="navbar-brand">
+            <h4>Display All books</h4>
+          </Link>
+        </nav>
+        <br />
+        <Switch>
+          <Route path="/" exact component={Book_Form} />
+          <Route path="/edit/:id" component={Book_UpdateForm} />
+          <Route path="/Delete/:id" component={DeleteBook} />
+          <Route path="/DisplayBooksF1" component={FncDisplayBooks} />
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
